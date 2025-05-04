59d8b49b5fb8a98c225772330f824756b5ee2e29
vchlinnn <chuclinh190305@gmail.com>
2025-05-03 23:11:37 -0500

    Add Day interface

---
+++ src/main/java/edu/grinnell/csc207/textadventure/Day.java
@@ -0,0 +1,13 @@
+package edu.grinnell.csc207.textadventure;
+
+public interface Day {
+
+    void playerWait();
+    void go();
+    void talk();
+    void pick();
+    void use();
+    void kill();
+    void wakeUp();
+
+}
