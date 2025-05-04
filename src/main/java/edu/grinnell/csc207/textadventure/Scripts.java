69fe7c3c8c0ae994aeac5e4e3c521f45a089695a
vchlinnn <chuclinh190305@gmail.com>
2025-05-04 00:23:18 -0500

    Add the scripts

---
+++ src/main/java/edu/grinnell/csc207/textadventure/Scripts.java
@@ -0,0 +1,15 @@
+package edu.grinnell.csc207.textadventure;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Scripts {
+    Map<String, List<String>> scripts;
+    
+    public Scripts() {
+        this.scripts = new HashMap<String,List<String>>();
+        scripts.put("open", Arrays.asList("It's locked.", "Good job, you unlocked it."));
+    }
+}
